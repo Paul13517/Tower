0c42e97ccdbf957867c32debfe03d1501ce780fb
Paul17135 <pavelfsn@yandex.ru>
2020-06-27 00:57:30 +0300

    Initial commit

---
+++ Assets/Scripts/CustomHandler.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using Vuforia;
+
+public class CustomHandler : MonoBehaviour
+{
+
+    public GameObject fadeTower;
+    public PlaneFinderBehaviour planeFinder;
+
+    public void handleAutomaticTest(HitTestResult result)
+    {
+        Renderer[] renderers = planeFinder.PlaneIndicator.GetComponentsInChildren<Renderer>(true);
+        Canvas[] canvas = planeFinder.PlaneIndicator.GetComponentsInChildren<Canvas>(true);
+
+        foreach (Renderer el in renderers)
+            el.enabled = false;
+
+        foreach (Canvas el in canvas)
+            el.enabled = false;
+
+        fadeTower.SetActive(true);
+        fadeTower.PositionAt(result.Position);
+    }
+
+}
+
---
+++ Assets/Scripts/TrackImage.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TrackImage : DefaultTrackableEventHandler {
+
+    public Sprite foundSprite, notFoundSprite; 
+    public Image image; 
+
+    public void changeTrackImage(bool isTracking) {
+         if(isTracking)
+             image.sprite = foundSprite;
+         else  
+             image.sprite = notFoundSprite;
+    }
+
+    protected override void OnTrackingFound() {
+        base.OnTrackingFound();
+        changeTrackImage(true);
+    }
+
+    protected override void OnTrackingLost() {
+        base.OnTrackingFound();
+        changeTrackImage(false);
+    }
+}
